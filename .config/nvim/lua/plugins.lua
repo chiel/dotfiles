fb7403c94a5c82927a62e5413dbda434ff2da67b
Chiel Kunkels <chiel@kunkels.me>
2022-01-14 23:29:52 +0100

    nvim: move telescope config to separate file

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -30,26 +30,7 @@ require('packer').startup(function()
 		requires = { 'nvim-lua/plenary.nvim' },
 		cmd = { 'Telescope' },
 		config = function()
-			require('telescope').setup{
-				defaults = {
-					file_ignore_patterns = {
-						"^.git/",
-					},
-					mappings = {
-						i = {
-							['<esc>'] = 'close',
-							['<c-j>'] = 'move_selection_next',
-							['<c-k>'] = 'move_selection_previous',
-						},
-					},
-				},
-				pickers = {
-					find_files = {
-						find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
-						hidden = true,
-					},
-				},
-			}
+			require('plugins/telescope')
 		end,
 	}
 
