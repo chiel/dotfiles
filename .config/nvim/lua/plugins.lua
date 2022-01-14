cf59a0d0f79fd5fd7099affdb3b47de888bd7b81
Chiel Kunkels <chiel@kunkels.me>
2022-01-14 23:40:33 +0100

    nvim: move tree config to separate file

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -52,22 +52,7 @@ require('packer').startup(function()
 		'kyazdani42/nvim-tree.lua',
 		cmd = { 'NvimTreeToggle', 'NvimTreeFindFile' },
 		config = function()
-			vim.g.nvim_tree_disable_window_picker = 1
-			vim.g.nvim_tree_git_hl = 1
-			vim.g.nvim_tree_show_icons = {
-				files = 1,
-				folder_arrows = 1,
-				folders = 1,
-				git = 0,
-			}
-			vim.g.nvim_tree_special_files = {}
-
-			require('nvim-tree').setup{
-				auto_close = true,
-				view = {
-					signcolumn = 'no',
-				},
-			}
+			require('plugins/tree')
 		end,
 	}
 
