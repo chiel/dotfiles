42960e06914d63b3b8720ead51d20cfa5dd39a55
Chiel Kunkels <chiel@kunkels.me>
2022-01-14 23:36:20 +0100

    nvim: move gitsigns config to separate file

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -38,12 +38,7 @@ require('packer').startup(function()
 		'lewis6991/gitsigns.nvim',
 		requires = { 'nvim-lua/plenary.nvim' },
 		config = function()
-			require('gitsigns').setup{
-				signs = {
-					add = { text = '▐' },
-					change = { text = '▐' },
-				},
-			}
+			require('plugins/gitsigns')
 		end,
 	}
 
