41725409ea7c39797af0ed35d8b78d204bddfb6d
Chiel Kunkels <chiel@kunkels.me>
2022-01-14 23:16:23 +0100

    nvim: move treesitter config to separate file

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -21,42 +21,7 @@ require('packer').startup(function()
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate',
 		config = function()
-			require('nvim-treesitter.configs').setup{
-				ensure_installed = {
-					'bash',
-					'comment',
-					'css',
-					'dockerfile',
-					'go',
-					'gomod',
-					'graphql',
-					'hcl',
-					'html',
-					'http',
-					'javascript',
-					'jsdoc',
-					'json',
-					'json5',
-					'lua',
-					'make',
-					'python',
-					'regex',
-					'ruby',
-					'scss',
-					'tsx',
-					'typescript',
-					'vim',
-					'yaml',
-				},
-
-				highlight = {
-					enable = true,
-				},
-
-				indent = {
-					enable = true,
-				},
-			}
+			require('plugins/treesitter')
 		end,
 	}
 
